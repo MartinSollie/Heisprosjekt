4e2b00eadcc8f88593f312d726ac27b9c53e4b97
MartinSollie <martin.lysvand.sollie@gmail.com>
2015-02-24 12:25:09 +0100

    fix edge trigger, i think ...

--- v2/main.c
+++ v2/main.c
@@ -16,7 +16,7 @@ int main(void){
     fsm_evSystemStarted();
 
     // Variables preventing button holding from continuosly adding new orders
-    bool btn_cmd[N_FLOORS] = {0};
+    bool btn_cmd[N_FLOORS] = {0;
     bool btn_up[N_FLOORS-1] = {0};
     bool btn_down[N_FLOORS-1] = {0};
 
@@ -24,37 +24,37 @@ int main(void){
 
     	//Poll buttons and add orders
     	for (int i = 0; i < N_FLOORS; i++){
-    		if(elev_get_button_signal(BUTTON_COMMAND, i) && btn_cmd[i] == false){
+    		if(elev_get_button_signal(BUTTON_COMMAND, i) && (btn_cmd[i] == false)){
     			addElevPanelOrder(i);
                 btn_cmd[i] = true;
                 if(getElevPanelFlag(i)){elev_set_button_lamp(BUTTON_COMMAND, i, 1);}
                 //printf("Detected command button press %d\n",i);
     		}
-            else{
+            else if (!elev_get_button_signal(BUTTON_COMMAND, i)){
                 btn_cmd[i] = false;
             }
 
 
     		if(i != 0){
-    			if(elev_get_button_signal(BUTTON_CALL_DOWN, i) && btn_down[i-1] == false){
+    			if(elev_get_button_signal(BUTTON_CALL_DOWN, i) && (btn_down[i-1] == false)){
     				addFloorPanelOrder(i, -1);
                     btn_down[i-1] = true;
                     if(getFloorPanelFlag(i,-1)){elev_set_button_lamp(BUTTON_CALL_DOWN, i, 1);}
                     //printf("Detected DOWN button press %d\n",i);
     			}
-                else{
+                else if (!elev_get_button_signal(BUTTON_CALL_DOWN, i)){
                     btn_down[i-1] = false;
                 }
     		}
             
     		if(i != N_FLOORS-1){
-    			if(elev_get_button_signal(BUTTON_CALL_UP, i) && btn_up[i] == false){
+    			if(elev_get_button_signal(BUTTON_CALL_UP, i) && (btn_up[i] == false)){
     				addFloorPanelOrder(i,1);
                     btn_up[i] = true;
                     if(getFloorPanelFlag(i,1)){elev_set_button_lamp(BUTTON_CALL_UP, i, 1);}
                     //printf("Detected UP button press %d\n",i);
     			}
-                else{
+                else if (!elev_get_button_signal(BUTTON_CALL_UP, i)){
                     btn_up[i] = false;
                 }
     		}
